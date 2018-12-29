e28df0876aa3232e618f5a891076d7e428cdcf63
anthony.albertorio <anthony.albertorio@gmail.com>
2018-12-29 02:08:17 -0500

    Updated src/App.js changed uPort to Blockstack

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <Button bsStyle='danger'>
-            Insert Uport
+            Insert Blockstack
           </Button>
         </header>
       </div>
