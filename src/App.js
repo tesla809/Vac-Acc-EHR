713518c94f71c41de0d07e20ed85da79c45a0a74
rene <renegmed@yahoo.com>
2019-02-11 09:22:51 -0500

    start of frontend dev

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
 import logo from './img/Vac-Acc-logo.png';
-import './App.css';
+import { Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
-    return (
+    return ( 
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Button bsStyle='danger'>
-            Insert Blockstack
-          </Button>
-        </header>
+       
+         <Route exact path="/" render={ () => (
+            <div>
+              Main Page
+            </div>    
+         )} />
+
+         <Route exact path="/Attestation" render={ () => (
+            <div>
+              Attestation Page
+            </div>    
+         )} /> 
       </div>
     );
   }
